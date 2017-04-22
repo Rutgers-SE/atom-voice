9e8e64c7820f767b54382a60c04787bf6bda136c
Alexander Sanchez <asanchez.dev@gmail.com>
2017-04-22 14:55:25 -0400

    init

---
+++ spec/atom-voice-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomVoiceView from '../lib/atom-voice-view';
+
+describe('AtomVoiceView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
